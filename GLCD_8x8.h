202f0aa54e168d14c89ac22e3ff3f7c46b20211d
KingsmanAbhi <130447785+KingsmanAbhi@users.noreply.github.com>
2023-07-11 11:44:01 +0530

    Add files via upload

---
+++ GLCD_8x8.h
@@ -0,0 +1,11 @@
+
+
+#ifndef GLCD_8X8_H
+#define	GLCD_8X8_H
+
+
+unsigned char get_width_of_char(unsigned char charecter);
+unsigned char get_data_from_data_table(unsigned char charecter, unsigned char col);
+
+#endif	/* GLCD_8X8_H */
+
